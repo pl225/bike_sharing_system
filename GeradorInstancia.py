11af2f54ebdcce4e06ff11f7ec719562caba2517
Matheus Abreu <matheusa596@gmail.com>
2018-08-03 13:07:07 -0300

    As instâncias criadas são salvas no formato .xml. Adição da propriedade de de distância Euclidiana em cada aresta.

--- GeradorInstancia.py
+++ GeradorInstancia.py
@@ -1,6 +1,12 @@
 from random import randint, choice
 from abc import abstractmethod
 from math import floor
+from math import sqrt
+
+def distanciaEuclidiana(x, y):
+	x1, x2 = x
+	y1, y2 = y
+	return sqrt((y1 - x1) ** 2 + (y2 - x2) ** 2)
 
 class GeradorInstanciaAbstrato ():
 	@abstractmethod
@@ -9,7 +15,7 @@ class GeradorInstanciaAbstrato ():
 
 	def gerarGrafo(self, n):
 		from graph_tool.all import complete_graph, graph_draw
-		from numpy import array
+		from numpy import array, vectorize
 
 		pontos, demandas, pis, qis = self.gerar(n)
 		
@@ -26,6 +32,16 @@ class GeradorInstanciaAbstrato ():
 
 		qisProperty = grafo.new_vertex_property("int") # cria uma propriedade para guardar o nº alvo de bicicletas de cada vértice
 		qisProperty.a = array(qis)
+
+		custoProperty = grafo.new_edge_property("float") # cria uma propriedade para guardar o custo de cada aresta
+		for e in grafo.edges():
+			custoProperty[e] = distanciaEuclidiana((pontosXProperty[e.source()], pontosYProperty[e.source()]), (pontosXProperty[e.target()], pontosYProperty[e.target()]))
+
+		grafo.vertex_properties["pi"] = pisProperty
+		grafo.vertex_properties["qi"] = qisProperty
+		grafo.edge_properties["custo"] = custoProperty
+
+		grafo.save("n{0}c{1}.xml".format(n, 1))
 
 		graph_draw(grafo, vertex_text = grafo.vertex_index, vertex_font_size=18)
 		
@@ -55,7 +71,7 @@ class GeradorPrimeiraClasse(GeradorInstanciaAbstrato):
 	def gerar(self, n):
 		pontos = [(0, 0)] + [(randint(GeradorPrimeiraClasse.espacoPosicao[0], GeradorPrimeiraClasse.espacoPosicao[1]),
 				 	randint(GeradorPrimeiraClasse.espacoPosicao[0], GeradorPrimeiraClasse.espacoPosicao[1])) for _ in range(n-1)]
-		demandas = [(randint(GeradorPrimeiraClasse.espacoDemanda[0], GeradorPrimeiraClasse.espacoDemanda[1])) for _ in range(n)]
+		demandas = [0] + [(randint(GeradorPrimeiraClasse.espacoDemanda[0], GeradorPrimeiraClasse.espacoDemanda[1])) for _ in range(n-1)]
 		pis = [10 for _ in range(n)]
 		qis = [10 + demandas[i] for i in range(n)]		
 
