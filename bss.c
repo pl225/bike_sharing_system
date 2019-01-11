ac6b06be678624ff449a56c2f914a071ac4b4937
Matheus Abreu <matheusa596@gmail.com>
2019-01-11 08:18:31 -0200

    Atualização dos casos no procedimento principal quando uma solução deve ser liberada.

--- bss.c
+++ bss.c
@@ -2,7 +2,6 @@
 #include "solucao.h"
 #include "vizinhanca.h"
 #include <time.h>
-
 int main(int argc, char *argv[])
 {
 	Grafo g = carregarInstancia("n20q10A.tsp");
@@ -15,17 +14,19 @@ int main(int argc, char *argv[])
 	FabricaSolucao fs = instanciarFabrica(g);
 	Solucao s, sLinha, sAsterisco, sAux;
 	float T, delta, x, fAsterisco = INFINITY;
-	int iterILS, lib_sLinha = 0;
+	int iterILS;
+	sAux.custo = INFINITY;
 
 	for (int i = 0; i < IR; i++) {
 		s = instanciarSolucao(fs);
-		sLinha = s; //copia
+		sLinha = s;
 		T = T0;
 		iterILS = 0;
 		
 		while (iterILS < iILS) {
-			s = RVND(s, fs);// antes de executar o RVND, s == sLinha
+			s = RVND(s, fs);
 			delta = s.custo - sLinha.custo;
+			if (sAux.custo != INFINITY && s.caminho != sAux.caminho){ liberarSolucao(sAux);}
 			if (delta < 0) {
 				liberarSolucao(sLinha);
 				sLinha = s;
@@ -33,17 +34,17 @@ int main(int argc, char *argv[])
 			} else {
 				x = ((double) rand() / (RAND_MAX));
 				if (T > 0 && x < exp(-(delta / T))) {
-					if (delta != 0) liberarSolucao(sLinha);
+					if (s.caminho != sLinha.caminho) liberarSolucao(sLinha);
 					sLinha = s;
-				} else {
+				} else if (s.caminho != sLinha.caminho) {
 					liberarSolucao(s);
 				}
 			}
 			s = perturbar(sLinha, fs);
+			sAux = s;
 			iterILS += 1;
 			T *= alpha;
 		}
-		liberarSolucao(s);
 		if (sLinha.custo < fAsterisco) {
 			if (fAsterisco != INFINITY) liberarSolucao(sAsterisco);
 			fAsterisco = sLinha.custo;
@@ -58,5 +59,6 @@ int main(int argc, char *argv[])
 	liberarGrafo(g);
 	liberarFabrica(fs);
 	liberarSolucao(sAsterisco);
+	liberarSolucao(s);
 	return 0;
 }
