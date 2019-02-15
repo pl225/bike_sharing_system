3ff44dc86c6b5a6d04f5d4bc20f999803d738e8a
Matheus Abreu <matheusa596@gmail.com>
2019-02-15 11:46:13 -0200

    Continuação da implementação da lista tabu. [Não testado]

--- tabu.c
+++ tabu.c
@@ -14,7 +14,7 @@ typedef struct
 
 typedef struct
 {
-	Conjunto* arcos;
+	Conjunto** arcos;
 	Movimento* movimentos;
 	int tamanho;
 	int nVertices;
@@ -32,8 +32,32 @@ ListaTabu criarListaTabu (int n, int nVertices) {
 	lista.indiceAtual = 0;
 	lista.movimentos = malloc(sizeof(Movimento) * n);
 	for (int i = 0; i < n; i++) lista.movimentos[i].n = 0;
-	lista.arcos = malloc(sizeof(Conjunto) * nVertices * nVertices);
+	lista.arcos = malloc(sizeof(Conjunto*) * nVertices * nVertices);
 	return lista;
+}
+
+ArcoPosicao criarArcoPosicao (int i, int j, int k) {
+	ArcoPosicao a;
+	a.i = i, a.j = j, a.k = k;
+	return a;
+}
+
+void preencherListaTabu (ListaTabu* lista, int* caminho, int tamanhoCaminho) {
+	
+	for (int i = 0; i < lista->nVertices * lista->nVertices; i++)
+			lista->arcos[i] = criarConjunto(tamanhoCaminho);
+	
+	Movimento* move = &lista->movimentos[0];
+	move->feitos = malloc(sizeof(ArcoPosicao) * (tamanhoCaminho - 1));
+	move->n = tamanhoCaminho - 1;
+	lista->indiceAtual = 1;
+	
+	int a = 0;
+	for (int i = 1; i < tamanhoCaminho; i++) {
+		a = i - 1;
+		set(lista->arcos[indConj(caminho[a], caminho[i], lista->nVertices)], a);
+		move->feitos[a] = criarArcoPosicao(caminho[a], caminho[i], a);
+	}
 }
 
 void AtualizarListaTabu (ListaTabu* lista, int* caminho, int tamanhoCaminho) {
@@ -45,23 +69,25 @@ void AtualizarListaTabu (ListaTabu* lista, int* caminho, int tamanhoCaminho) {
 		free(move->feitos);
 	}
 
-	move->feitos = malloc(sizeof(ArcoPosicao) * tamanhoCaminho);
-	Conjunto c;
-	int a = 0;
+	move->feitos = malloc(sizeof(ArcoPosicao) * (tamanhoCaminho - 1));
+	Conjunto* c;
+	int a = 0, b = 0;
 	for (int i = 1; i < tamanhoCaminho; i++) {
-		c = lista->arcos[indConj(caminho[i-1], caminho[i], lista->nVertices)];
-		if (!get(c, i-1)) {
-			set(c, i-1);
-			move->feitos[a] = {caminho[i-1], caminho[i], i-1};
+		b = i - 1;
+		c = lista->arcos[indConj(caminho[b], caminho[i], lista->nVertices)];
+		if (!get(c, b)) {
+			set(c, b);
+			move->feitos[a] = criarArcoPosicao(caminho[b], caminho[i], b);
 			a++;
 		}
 	}
-	move->feitos = realloc(move->feitos, sizeof(Movimento) * a);
-	lista->indiceAtual += 1;
+	move->feitos = realloc(move->feitos, sizeof(ArcoPosicao) * a);
+	move->n = a;
+	lista->indiceAtual = lista->indiceAtual + 1 >= lista->tamanho ? 0 : lista->indiceAtual + 1;
 }
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	
 	return 0;
 }
