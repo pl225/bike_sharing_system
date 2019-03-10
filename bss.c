c85775d18b3d0d541920196dd9b439164a3ea7ba
Matheus Abreu <matheusa596@gmail.com>
2019-03-10 16:52:08 -0300

    Personalização do procedimento principal para ser excutado várias vezes.

--- bss.c
+++ bss.c
@@ -2,21 +2,16 @@
 #include "solucao.h"
 #include "vizinhanca.h"
 #include <time.h>
-int main(int argc, char *argv[])
-{
-	char caminho[25];
-	strcpy(caminho, "instancias/");
-	strcat(caminho, argv[1]);
+
+void ILS_SBPRW (Grafo g, FabricaSolucao fs, FILE* arq, float results[]) {
 	
-	Grafo g = carregarInstancia(caminho);
-	g.q = atoi(argv[2]);
+	clock_t start = clock();
+	
 	int IR = 100;
 	int iILS = 10 * g.n;
 	float alpha = 0.75;
 	float T0 = 1000;
 	
-	srand(time(NULL));
-	FabricaSolucao fs = instanciarFabrica(g);
 	Solucao s, sLinha, sAsterisco, sAux;
 	float T, delta, x, fAsterisco = INFINITY;
 	int iterILS;
@@ -59,11 +54,61 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	imprimirSolucao(sAsterisco, fs);
-
-	liberarGrafo(g);
-	liberarFabrica(fs);
-	liberarSolucao(sAsterisco);
+	if (fAsterisco != INFINITY) liberarSolucao(sAsterisco);
 	liberarSolucao(s);
+
+	clock_t end = clock();
+	float seconds = (float) (end - start) / CLOCKS_PER_SEC;
+	
+	results[0] = fAsterisco, results[1] = seconds;
+}
+
+int main(int argc, char *argv[])
+{
+
+	char *ns [] = {"20", "30", "40", "50", "60"};
+	char *grupo [] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	int qs [] = {10, 15, 20, 25, 30, 35, 40, 45, 1000};
+	float results[2]; // 0 = custo, 1 = tempo
+
+	FILE *arq = fopen(argv[1], "w");
+
+	char caminho[30];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 10; j++) {
+
+			strcpy(caminho, "instancias/");
+			strcat(caminho, "n");
+			strcat(caminho, ns[i]);
+			strcat(caminho, "q10");
+			strcat(caminho, grupo[j]);
+			strcat(caminho, ".tsp");
+
+			Grafo g = carregarInstancia(caminho);
+			FabricaSolucao fs = instanciarFabrica(g);
+
+			for (int k = 0; k < 9; k++) {
+				
+				g.q = qs[k];
+				fs.q = qs[k];
+
+				fprintf(arq, "%s %d\n", caminho, qs[k]);
+				double mediaTempo = 0;
+
+				for (int l = 0; l < 10; l++) {
+					ILS_SBPRW(g, fs, arq, results);
+					fprintf(arq, "\tCusto%s\n", );
+				}				
+			}
+			liberarGrafo(g);
+			liberarFabrica(fs);
+			strcpy(caminho, "");
+		}
+	}
+
+	fclose(arq);
 	return 0;
 }
