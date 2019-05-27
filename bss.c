45868f72f24fe2fb71e4326cb69a7a934556e41f
Matheus Abreu <matheusa596@gmail.com>
2019-05-26 21:46:16 -0300

    Implementação do procedimento principal.

--- bss.c
+++ bss.c
@@ -21,18 +21,49 @@
 
 int main(int argc, char *argv[])
 {
-	
 	srand(time(NULL));
-	Grafo g = carregarInstancia("instancias/teste_teo.tsp");
+	Grafo g = carregarInstancia("instancias/n60q10A.tsp");
 	FabricaSolucao fs = instanciarFabrica(g);
 
-	Solucao s = GRASP(fs);
+	Solucao x = GRASP(fs), xLinha, xLinhaLinha;
 
-	imprimirSolucao(s, fs);
+	const int kMax = 2;
+	int k, maxIteracoes = 10 * g.n, i = 0;
+	float melhorCusto = INFINITY;
+	
+	do {
+		k = 1;
+
+		do {
+			xLinha = k == 0 ? _3OPT_P(x, fs) : splitP(x, fs);
+			xLinhaLinha = RVND(xLinha, fs);
+
+			if (xLinha.caminho != xLinhaLinha.caminho) liberarSolucao(xLinha);
+			
+			if (xLinhaLinha.custo < x.custo && isViavel(xLinhaLinha)) {
+				liberarSolucao(x);
+				x = xLinhaLinha;
+				k = 1;
+			} else {
+				k += 1;
+				if (xLinha.caminho != xLinhaLinha.caminho) liberarSolucao(xLinhaLinha);
+			}
+		} while (k < kMax);
+
+		if (melhorCusto < x.custo) {
+			i = 0;
+			melhorCusto = x.custo;
+		} else {
+			i++;
+		}
+
+	} while (i < maxIteracoes);
+
+	imprimirSolucao(x, fs);
 
 	liberarGrafo(g);
 	liberarFabrica(fs);
-	liberarSolucao(s);
+	liberarSolucao(x);
 
 	return 0;
 }
